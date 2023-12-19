274dad4ab70022177b5561b609f05e728409adde
moelt-mb <mmark5437@yahoo.com>
2023-12-18 21:03:24 -0800

    added proper constants

--- main.c
+++ main.c
@@ -3,15 +3,11 @@
 int main(int argc, char** argv) {
     int num_equations = 3;
 
-    double a = 1.0;
-    double b = 1.0;
-    double c = 1.0;
+    double sigma = 10;
+    double beta = 8.0/3;
+    double rho = 28;
 
-    double emf = 10;
-    double capacity = 1;
-    double resistance = 2;
-
-    runge(num_equations, a, b, c);
+    runge(num_equations, sigma, beta, rho);
 
     return 0;
 }
--- rk45.c
+++ rk45.c
@@ -1,28 +1,30 @@
 #include "rk45.h"
 
-double* calculate_k(double x, double h, const double* y_input, const size_t n, 
-                    double emf, double capacity, double resistance) {
+double* calculate_k(double t, double h, const double* input, const size_t n, 
+                    double sigma, double beta, double rho) {
     double f[n];
+    double x = input[0], y = input[1], z = input[2];
 
     // functions here
-    f[0] = (emf - y_input[0]/capacity) / resistance;
-    f[1] = -y_input[1] / (capacity * resistance);
+    f[0] = sigma*(y - x);
+    f[1] = x*(rho - z) - y;
+    f[2] = x*y - beta*z;
 
     return num_times_array(h, f, n); 
 }
 
 double* rk_step(double t, double h, double* y, const size_t n,
-             double emf, double capacity, double resistance){
+             double sigma, double beta, double rho){
     double *dy;
     double *k1, *k2, *k3, *k4;
 
     dy = malloc(sizeof(double) * n);
 
     // ugly array operations
-    k1 = calculate_k(t, h, y, n, emf, capacity, resistance);
-    k2 = calculate_k(t+h/2, h, add_arrays(y, num_times_array(0.5, k1, n), n), n, emf, capacity, resistance);
-    k3 = calculate_k(t+h/2, h, add_arrays(y, num_times_array(0.5, k2, n), n), n, emf, capacity, resistance);
-    k4 = calculate_k(t+h, h, add_arrays(y, k3, n), n, emf, capacity, resistance);
+    k1 = calculate_k(t, h, y, n, sigma, beta, rho);
+    k2 = calculate_k(t+h/2, h, add_arrays(y, num_times_array(0.5, k1, n), n), n, sigma, beta, rho);
+    k3 = calculate_k(t+h/2, h, add_arrays(y, num_times_array(0.5, k2, n), n), n, sigma, beta, rho);
+    k4 = calculate_k(t+h, h, add_arrays(y, k3, n), n, sigma, beta, rho);
 
     // Copy k1 to dy
     for (int i = 0; i < n; i++) dy[i] = k1[i];
@@ -36,24 +38,31 @@ double* rk_step(double t, double h, double* y, const size_t n,
     return dy; 
 }
 
-void runge(int num_equations, double a, double b,
-             double c){
+void runge(int num_equations, double sigma, double beta,
+             double rho){
+    FILE *fptr;
+
     double t, dt, t_max;
     double y[num_equations];
 
-    y[0] = 0;
-    y[1] = emf / resistance;
+    y[0] = 1.0;   // x, y, z
+    y[1] = 1.0;
+    y[2] = 1.0;
 
     t = 0;
-    dt = 0.1;
-    t_max = 10;
+    dt = 0.05;
+    t_max = 75;
+
+    fptr = fopen("lorentz.txt", "w");
 
     while (t < t_max) {
         double *dy;
 
-        printf("%.16f\t%.16f\t%.16f\n", t, y[0], y[1]);
-        dy = rk_step(t, dt, y, num_equations, emf, capacity, resistance);
+        fprintf(fptr, "%.16f\t%.16f\t%.16f\n", y[0], y[1], y[2]);
+        dy = rk_step(t, dt, y, num_equations, sigma, beta, rho);
         add_array_to_array(y, dy, num_equations);
         t += dt;
     }
+
+    fclose(fptr);
 }
